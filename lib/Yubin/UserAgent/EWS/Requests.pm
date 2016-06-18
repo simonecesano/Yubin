86f47cce9706fef62525e104659c9cf6726f1d0f
Simone Cesano <simonecesano@libero.it>
2016-06-18 09:52:23 +0200

    first app commit

--- lib/Yubin/UserAgent/EWS/Requests.pm
+++ lib/Yubin/UserAgent/EWS/Requests.pm
@@ -13,10 +13,10 @@ sub compile {
     if (-f $template) {
 	$template = path($template)->slurp
     } else {
-	print STDERR "#2 $template";
-	print STDERR "#p " . __PACKAGE__;
+	# print STDERR "#2 $template";
+	# print STDERR "#p " . __PACKAGE__;
 	$template = ${__PACKAGE__->section_data($template)};
-	print STDERR "#3 $template";
+	# print STDERR "#3 $template";
     };
     my $text = $self->tx->render_string($template, $data);
     return $text;
