e2a81add03948f294064fc4b2ac5457521a4f3e5
Simone Cesano <simonecesano@libero.it>
2016-09-17 10:23:25 -0400

    Added prereqs

--- Makefile.pl
+++ Makefile.pl
@@ -1,5 +1,24 @@
 use ExtUtils::MakeMaker;
 WriteMakefile(
 	      NAME            => 'Yubin',
-	      VERSION_FROM    => 'lib/Yubin.pm'
+	      VERSION_FROM    => 'lib/Yubin.pm',
+	      PREREQ_PM       => {
+				  "Authen::NTLM" => 0,
+				  "Data::Dump" => 0,
+				  "Data::Section" => 0,
+				  "Data::Section::Simple" => 0,
+				  "ExtUtils::MakeMaker" => 0,
+				  "HTTP::Request" => 0,
+				  "HTTP::Response" => 0,
+				  "JSON" => 0,
+				  "LWP::UserAgent" => 0,
+				  "Moose::Role" => 0,
+				  "Moose" => 0,
+				  "Path::Tiny" => 0,
+				  "Sub::Exporter::ForMethods" => 0,
+				  "Text::Xslate" => 0,
+				  "Want" => 0,
+				  "XML::Bare" => 0,
+				  "XML::LibXML" => 0,
+				  }
 	     );
